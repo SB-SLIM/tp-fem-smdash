2b16c6ce6543c11816113db46a941eae3d545586
B <sbslim.dev@gmail.com>
2021-09-19 13:22:48 +0100

    feat: mode dark light

--- src/components/MiniCards/MiniCard.js
+++ src/components/MiniCards/MiniCard.js
@@ -12,20 +12,20 @@ export default function MiniCard({
 }) {
   return (
     <div className="card">
-      <div className="card-body ">
-        <div className="card-color d-flex justify-content-between align-items-center">
-          <p className="ms-1">{txtFollowers}</p>
+      <div className="card-body">
+        <div className="card-color mb-4 d-flex justify-content-between align-items-center">
+          <p>{txtFollowers}</p>
           <img className="card-image" src={img} alt={alt} />
         </div>
         <div className="d-flex justify-content-between align-items-center">
           <p className="fw-bold">{nbrFollowers}</p>
           {dir === "up" ? (
-            <div className="text-secondary ">
+            <div className="text-secondary d-flex justify-content-center align-items-center">
               <img className="card-image " src={up} alt="up" />
-              <p className="ms-1 text-primary">{percent}%</p>
+              <p className="text-primary">{percent}%</p>
             </div>
           ) : (
-            <div className="text-secondary ">
+            <div className="text-secondary d-flex justify-content-center align-items-center">
               <img className="card-image" src={down} alt="down" />
               <p className="text-secondary">{percent}%</p>
             </div>
