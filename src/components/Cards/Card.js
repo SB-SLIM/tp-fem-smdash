9d667f4c69e0c762e2a2982e29acd618c6d16e75
B <sbslim.dev@gmail.com>
2021-09-20 17:06:58 +0100

    fix linear-gradient

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -11,32 +11,33 @@ export default function Card({
   dir = "up",
   borderColor,
 }) {
-  let className =
-    "card card-border m-1 col-2 text-center card-border-color-" + borderColor;
+  let className = "card-border-size m-4  card-border-color-" + borderColor;
   return (
     <div className={className}>
-      <div className="card-body">
-        <div className="card-color d-flex justify-content-center ">
-          <img className="card-image" src={img} alt={alt} />
-          <p className="ms-1">{nameUser}</p>
-        </div>
-        <div>
-          <h1 className="fw-bold">{nbrFollowers}</h1>
-          <p className="card-color text-uppercase letter-spacing">
-            {txtFollowers}
-          </p>
-        </div>
-        {dir === "up" ? (
-          <div className="text-secondary d-flex justify-content-center align-items-center">
-            <img className="card-image " src={up} alt="up" />
-            <h4 className="ms-1 text-primary">Today</h4>
+      <div className="card card-border-color-margin text-center">
+        <div className="card-body">
+          <div className="card-color d-flex justify-content-center ">
+            <img className="card-image" src={img} alt={alt} />
+            <p className="ms-1">{nameUser}</p>
           </div>
-        ) : (
-          <div className="text-secondary d-flex justify-content-center align-items-center">
-            <img className="card-image" src={down} alt="down" />
-            <h4 className="ms-1 text-secondary">Today</h4>
+          <div>
+            <h1 className="fw-bold">{nbrFollowers}</h1>
+            <p className="card-color text-uppercase letter-spacing">
+              {txtFollowers}
+            </p>
           </div>
-        )}
+          {dir === "up" ? (
+            <div className="text-secondary d-flex justify-content-center align-items-center">
+              <img className="card-image " src={up} alt="up" />
+              <h4 className="ms-1 text-primary">Today</h4>
+            </div>
+          ) : (
+            <div className="text-secondary d-flex justify-content-center align-items-center">
+              <img className="card-image" src={down} alt="down" />
+              <h4 className="ms-1 text-secondary">Today</h4>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/components/MiniCards/MiniCard.js
+++ src/components/MiniCards/MiniCard.js
@@ -11,25 +11,27 @@ export default function MiniCard({
   percent,
 }) {
   return (
-    <div className="card">
-      <div className="card-body">
-        <div className="card-color mb-4 d-flex justify-content-between align-items-center">
-          <p>{txtFollowers}</p>
-          <img className="card-image" src={img} alt={alt} />
-        </div>
-        <div className="d-flex justify-content-between align-items-center">
-          <p className="fw-bold">{nbrFollowers}</p>
-          {dir === "up" ? (
-            <div className="text-secondary d-flex justify-content-center align-items-center">
-              <img className="card-image " src={up} alt="up" />
-              <p className="text-primary">{percent}%</p>
-            </div>
-          ) : (
-            <div className="text-secondary d-flex justify-content-center align-items-center">
-              <img className="card-image" src={down} alt="down" />
-              <p className="text-secondary">{percent}%</p>
-            </div>
-          )}
+    <div className=" card-border-size m-4">
+      <div className="card ">
+        <div className="card-body">
+          <div className="card-color mb-4 d-flex justify-content-between align-items-center">
+            <p>{txtFollowers}</p>
+            <img className="card-image" src={img} alt={alt} />
+          </div>
+          <div className="d-flex justify-content-between align-items-center">
+            <p className="fw-bold">{nbrFollowers}</p>
+            {dir === "up" ? (
+              <div className="text-secondary d-flex justify-content-center align-items-center">
+                <img className="card-image " src={up} alt="up" />
+                <p className="text-primary">{percent}%</p>
+              </div>
+            ) : (
+              <div className="text-secondary d-flex justify-content-center align-items-center">
+                <img className="card-image" src={down} alt="down" />
+                <p className="text-secondary">{percent}%</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
